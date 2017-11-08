0cfeb753b1866ec49978d970a396d3460d5fb1ff
gezhen.gz <gezhen.gz@alibaba-inc.com>
2017-11-08 22:28:49 +0800

    nyu操作方法

--- read_nyu_image.py
+++ read_nyu_image.py
@@ -1,13 +1,22 @@
 import cv2
+import h5py
 import numpy as np
+
 filepath = 'nyu_depth_v2_labeled.mat'
 arrays = {}
 f = h5py.File(filepath)
 # print(f.keys())
-for k,v in f.items():
+for k, v in f.items():
     arrays[k] = v
 
 images = arrays['images']
-first_pic = np.transpose(images[0], [2,1,0]) # images[0].shape -> Nx3xWxH
-first_pic = first_pic[:,:,[2,1,0]] # first_pic是bgr 需要转成rgb
-# cv2.imwrite('test.png',first_pic)
+first_pic = np.transpose(images[0], [2, 1, 0])  # images[0].shape -> Nx3xWxH
+first_pic = first_pic[:, :, [2, 1, 0]]  # first_pic是bgr 需要转成rgb
+cv2.imwrite('test.png', first_pic)
+
+depths = arrays['depths']
+d = depths[0]
+d = (d - 1.5) / 2.5 * 255  # 因为这个图的范围在1.7-3.6 hardcode一下
+first_depth = np.transpose(d, [1, 0]).astype('uint8')
+first_depth = cv2.applyColorMap(first_depth, cv2.COLORMAP_JET)
+cv2.imwrite("./nyu_depth_test.jpg", first_depth)
